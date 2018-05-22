151af5ecd85f7100c5e3dabd3b06613cd7c63688
Nikita Bakulin <bakulin.nikita@bk.ru>
2018-05-22 22:11:31 +0500

    added TestControlsApplication

---
+++ TestControlsApplication/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TestControlsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.colorSelector1 = new LabControls.ColorSelector();
+            this.filePathSelect1 = new LabControls.FilePathSelect();
+            this.numberBox1 = new LabControls.NumberBox(this.components);
+            this.SuspendLayout();
+            // 
+            // colorSelector1
+            // 
+            this.colorSelector1.Location = new System.Drawing.Point(416, 12);
+            this.colorSelector1.Name = "colorSelector1";
+            this.colorSelector1.Size = new System.Drawing.Size(238, 166);
+            this.colorSelector1.TabIndex = 3;
+            this.colorSelector1.Load += new System.EventHandler(this.colorSelector1_Load);
+            // 
+            // filePathSelect1
+            // 
+            this.filePathSelect1.FileName = "";
+            this.filePathSelect1.Location = new System.Drawing.Point(12, 86);
+            this.filePathSelect1.Name = "filePathSelect1";
+            this.filePathSelect1.Size = new System.Drawing.Size(388, 50);
+            this.filePathSelect1.TabIndex = 1;
+            // 
+            // numberBox1
+            // 
+            this.numberBox1.ForeColor = System.Drawing.Color.Red;
+            this.numberBox1.Location = new System.Drawing.Point(27, 44);
+            this.numberBox1.Name = "numberBox1";
+            this.numberBox1.Size = new System.Drawing.Size(356, 20);
+            this.numberBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 341);
+            this.Controls.Add(this.colorSelector1);
+            this.Controls.Add(this.filePathSelect1);
+            this.Controls.Add(this.numberBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LabControls.NumberBox numberBox1;
+        private LabControls.FilePathSelect filePathSelect1;
+        private LabControls.ColorSelector colorSelector1;
+    }
+}
+
